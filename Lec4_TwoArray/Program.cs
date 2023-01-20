aa7e137c6b3283913cbcdf6a4aa83ba67ad301e6
kategitt <kapitanEka@yandex.ru>
2023-01-20 16:17:00 +0300

    Рекурсия - метод, который вызывает сам себя. Записала все с лекции

--- Lec4_TwoArray/Program.cs
+++ Lec4_TwoArray/Program.cs
@@ -1,2 +1,129 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [,] table = new string [2, 5];
+
+// инициализация строк: String.Empty
+
+// table[1, 2] = "слово";
+// for (double rows = 0; rows < 2; rows++)
+// {
+//     for (double columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+// void PrdoubleArray(double[,] image)
+// {
+//     for (double i = 0; i < image.GetLength(0); i++) // 0 - количество строк. 3 то есть.
+//     {
+//         for (double j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} ");
+//         }
+//     Console.WriteLine();    
+//     }
+// }
+// void FillArray(double[,] image)
+// {
+//     for (double i = 0; i < image.GetLength(0); i++) 
+//     {
+//         for (double j = 0; j < image.GetLength(1); j++)
+//         {
+//             image[i, j] = new Random().Next(1, 10);
+//         }
+
+//     }
+// }
+// double[,] imageix = new double[3, 4];
+// PrdoubleArray(imageix);
+// FillArray(imageix);
+// Console.WriteLine();
+// PrdoubleArray(imageix);
+
+// метод, который будет выводить картинку закрашиваемую
+// double[,] pic = new double[,]
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void PrdoubleImage(double[,] image)
+// {
+//     for (double i = 0; i < image.GetLength(0); i++) // 0 - количество строк. 3 то есть.
+//     {
+//         for (double j = 0; j < image.GetLength(1); j++)
+//         {
+//             if(image[i, j] == 0) Console.Write($" ");
+//             else Console.Write($"+");
+//         }
+//     Console.WriteLine();    
+//     }
+// }
+// void FillImage (double row, double col)
+// {
+//     if(pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row-1, col);
+//         FillImage(row, col-1);
+//         FillImage(row+1, col);
+//         FillImage(row, col+1);
+//     }
+// }
+// PrdoubleImage(pic);
+// FillImage(13, 13);
+// PrdoubleImage(pic);
+
+// код дл вывода факториала больших чисел. Для маленьких (до 17) double.
+
+// double Factorial (double n)
+// {
+//     //1! = 1
+//     if(n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (double i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}"); //1*2*3 =6
+// }
+
+// Числа Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+// double Fibonacci(int n)
+// {
+//     if(n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
+
+// чтобы обойти директорию, надо пройти все папки
+// и показать содержимое папок
